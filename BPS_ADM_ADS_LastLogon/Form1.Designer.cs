2f44bf5c1809d4facda81f70b9efc92542f450b1
DanielT <DanielT@2016-DANIEL-SP>
2016-11-16 10:46:19 +0100

    Projektdateien hinzufügen.

---
+++ BPS_ADM_ADS_LastLogon/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace BPS_ADM_ADS_LastLogon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Hostname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LogonTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Hostname,
+            this.LogonTime});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(896, 347);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Hostname
+            // 
+            this.Hostname.HeaderText = "Hostname";
+            this.Hostname.Name = "Hostname";
+            // 
+            // LogonTime
+            // 
+            this.LogonTime.HeaderText = "Logon Time";
+            this.LogonTime.Name = "LogonTime";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(921, 371);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Last Computer Logons";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Hostname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LogonTime;
+    }
+}
+
---
+++ BPS_ADM_ADS_LastLogon/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
+using System.DirectoryServices.ActiveDirectory;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BPS_ADM_ADS_LastLogon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getLastLogonUser();
+        }
+
+        public void getLastLogonUser()
+        { 
+            using (var context = new PrincipalContext(ContextType.Domain, "bps.bps-software.de"))
+        {
+            using (var searcher = new PrincipalSearcher(new ComputerPrincipal(context)))
+            {
+                foreach (var result in searcher.FindAll())
+                {
+                    var auth = result as AuthenticablePrincipal;
+                    if(auth != null)
+                    {
+                       dataGridView1.Rows.Add(dataGridView1.Rows.auth.Name
+                                auth.LastLogon
+                    }
+                }
+            }
+        }
+        Console.ReadLine();
+        }
+
+        public DateTime findlastlogon(string userName)
+
+        {
+            DirectoryContext context = new DirectoryContext(DirectoryContextType.Domain, "domainName");
+            DateTime latestLogon = DateTime.MinValue;
+            DomainControllerCollection dcc = DomainController.FindAll(context);
+            Parallel.ForEach(dcc.Cast<object>(), dc1 =>
+            {
+
+
+                DirectorySearcher ds;
+                DomainController dc = (DomainController)dc1;
+                using (ds = dc.GetDirectorySearcher())
+                {
+                    try
+                    {
+                        ds.Filter = String.Format(
+                          "(sAMAccountName={0})",
+                          userName
+                          );
+                        ds.PropertiesToLoad.Add("lastLogon");
+                        ds.SizeLimit = 1;
+                        SearchResult sr = ds.FindOne();
+
+                        if (sr != null)
+                        {
+                            DateTime lastLogon = DateTime.MinValue;
+                            if (sr.Properties.Contains("lastLogon"))
+                            {
+                                lastLogon = DateTime.FromFileTime(
+                                  (long)sr.Properties["lastLogon"][0]
+                                  );
+                            }
+
+                            if (DateTime.Compare(lastLogon, latestLogon) > 0)
+                            {
+                                latestLogon = lastLogon;
+                                //servername = dc1.Name;
+                            }
+                        }
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+            });
+            return latestLogon;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
