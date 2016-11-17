43f1fd7a78617a6cc759345a89e123851b62c472
Ra1D3R <ra1d3r@web.de>
2016-11-17 19:19:01 +0100

    repair

--- DTTK_ADS_LastLogon/Form1.Designer.cs
+++ DTTK_ADS_LastLogon/Form1.Designer.cs
@@ -31,42 +31,82 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Hostname = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.LogonTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.StatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.StatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Hostname,
             this.LogonTime});
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(896, 347);
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(896, 334);
             this.dataGridView1.TabIndex = 1;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // Hostname
             // 
+            this.Hostname.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCellsExceptHeader;
             this.Hostname.HeaderText = "Hostname";
             this.Hostname.Name = "Hostname";
+            this.Hostname.Width = 5;
             // 
             // LogonTime
             // 
+            this.LogonTime.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCellsExceptHeader;
             this.LogonTime.HeaderText = "Logon Time";
             this.LogonTime.Name = "LogonTime";
+            this.LogonTime.Width = 5;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StatusLabel1,
+            this.StatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 349);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(921, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // StatusLabel1
+            // 
+            this.StatusLabel1.Name = "StatusLabel1";
+            this.StatusLabel1.Size = new System.Drawing.Size(42, 17);
+            this.StatusLabel1.Text = "Status:";
+            // 
+            // StatusLabel2
+            // 
+            this.StatusLabel2.Name = "StatusLabel2";
+            this.StatusLabel2.Size = new System.Drawing.Size(0, 17);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(921, 371);
+            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Last Computer Logons";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -75,6 +115,9 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Hostname;
         private System.Windows.Forms.DataGridViewTextBoxColumn LogonTime;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel StatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel StatusLabel2;
     }
 }
 
---
+++ DTTK_ADS_LastLogon/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
+using System.DirectoryServices.ActiveDirectory;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BPS_ADM_ADS_LastLogon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getLastLogonUser();
+        }
+
+        public void getLastLogonUser()
+        { 
+            using (var context = new PrincipalContext(ContextType.Domain, "bps.bps-software.de"))
+            {
+                using (var searcher = new PrincipalSearcher(new ComputerPrincipal(context)))
+                {
+                    int NumHosts = 0;
+                    int CountRows = 0;
+                    int CountEmpty = 0;
+                    foreach (var result in searcher.FindAll())
+                    {
+                        var auth = result as AuthenticablePrincipal;
+                        if(auth != null)
+                        {
+
+                            if (auth.LastLogon != null)
+                            {
+                                this.dataGridView1.Rows.Add(auth.Name, auth.LastLogon);
+                                CountRows++;
+                            }
+                            else CountEmpty++;
+                            NumHosts = dataGridView1.RowCount + CountEmpty;
+                            StatusLabel2.Text = "Hosts: " + NumHosts + " | Hosts mit Logon-Datum: " + CountRows + " | Hosts ohne Logon-Datum: " + CountEmpty;
+                        }
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
